d935e9ef22043cd48b741550bfb2b10e473e57c3
andreasmz <174248270+andreasmz@users.noreply.github.com>
2024-07-09 13:30:10 +0200

    gui dev

--- dev/roi_detector.py
+++ dev/roi_detector.py
@@ -1,16 +1,87 @@
 import tkinter as tk
+from tkinter import ttk
+import imagej
+import threading
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
+def StartImageJ():
+    global ij
+    path1 = "C:\\Users\\abril\\Desktop\\Arbeitsprogramme\\fiji-win64\\Fiji.app"
+    path2 = "C:\\Users\\abril\\Andreas Eigene Dateien\\Programme\\Fiji.app"
 
+    ij = imagej.init(path1, mode='interactive')
+    ij.ui().showUI()
+    menuImageJ.entryconfig("Read Image", state="normal")
+    progStartingImgJ.pack_forget()
+    lblStartImageJ.pack_forget()
+
+    lblImgInfo["text"] = "No image selected"
+    lblImgInfo.pack()
+
+def MbtnStartImageJ_Click():
+    lblStartImageJ.pack(side=tk.LEFT)
+
+    menuImageJ.entryconfig("Start ImageJ", state="disabled")
+
+    def ProgStartingImgJ_Step():
+        progStartingImgJ.step(10)
+        root.after(50, ProgStartingImgJ_Step)
+    progStartingImgJ.pack(side=tk.LEFT)
+    ProgStartingImgJ_Step()
+    
+    t = threading.Thread(target=StartImageJ)
+    t.start()
+   
+def MBtnReadImage():
+    global img
+    img = ij.py.active_xarray() 
+    if img is None:
+        return
+    img = np.array(img)
+    if (np.max(img) <= 2**15):
+        img = img.astype("int16")
+    if img is not None:
+        lblImgInfo["text"] = f"{img.shape}, dtype={img.dtype}"
+        ImageProcessing()
+    else:
+        lblImgInfo["text"] = "No image selected"
+
+def ImageProcessing():
+    global imgDiff, imgDiffMax
+    imgDiff = np.diff(img, axis=0)
+    imgDiffMax = np.max(imgDiff,axis=0)
+
+def Test():
+    ax1.clear()
+    ax1.imshow(imgDiffMax)
+    canvas.draw()
 
 root = tk.Tk()
 root.title("Glutamte Image ROI Detector")
 
-mainMenu = tk.Menu()
-root.config(menu=mainMenu)
-menuImageJ = tk.Menu(mainMenu)
-mainMenu.add_cascade("ImageJ",menu=menuImageJ)
+menubar = tk.Menu(root)
+root.config(menu=menubar)
+menuImageJ = tk.Menu(menubar,tearoff=0)
+menubar.add_cascade(label="ImageJ",menu=menuImageJ)
 
-menuImageJ.add_command(label="Read Image")
+menuImageJ.add_command(label="Start ImageJ", command=MbtnStartImageJ_Click)
+menuImageJ.add_command(label="Read Image",state="disabled", command=MBtnReadImage)
+menuImageJ.add_command(label="Test",state="normal", command=Test)
+
+progStartingImgJ = ttk.Progressbar(root,orient="horizontal", mode="indeterminate", length=200)
+lblStartImageJ = tk.Label(root,text="Starting ImageJ...")
+lblImgInfo = tk.Label(root)
+
+
+figure = plt.Figure(figsize=(6,6), dpi=100)
+ax1 = figure.add_subplot()  
+canvas = FigureCanvasTkAgg(figure, root)
+canvas.get_tk_widget().pack()
+
 
 root.mainloop()
 
